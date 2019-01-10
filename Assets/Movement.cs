9c0acb62a0722f5f9562c30cafc407312746e544
skyler <skyleryoung1111@gmail.com>
2019-01-10 15:07:16 -0700

    coding

---
+++ Assets/Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour {
+    //The multiplier for the movement, amount in frames
+    public float speed = 10.0f;
+    //the vertical multiplyier, frames
+    public float jumpSpeed = 8.0f;
+    //The pull of gravity
+    public float gravity = 20.0f;
+    public float turnspeed = 10.0f;
+    public Scrollbar Scrollbar_MouseSpeed;
+    //set a variable to save the location
+    private Vector3 moveDirection = Vector3.zero;
+    //the variable to let us get the character controller input
+    private CharacterController controller;
+
+    void Start()
+    {
+        //the setting of our variable to a dynamic variable
+        controller = GetComponent<CharacterController>();
+
+        // spawn location
+        gameObject.transform.position = new Vector3(0, 2, 0);
+    }
+    //frame by frame updater
+    void Update()
+    {
+
+        //test to see if object is colliding underneath
+        if (controller.isGrounded)
+        {
+            //use the character control in move direction
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity by removing the force of gravity times time.
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+        //mouselook
+        float mouse_x = Input.GetAxis("Mouse X");
+        //mybad
+        //float mouse_y = Input.GetAxis("Mouse Y"); i need to put this on the camera or something...idk.
+        Vector3 lookhere = new Vector3(0, mouse_x*turnspeed*Scrollbar_MouseSpeed.value, 0);
+        transform.Rotate(lookhere);
+
+
+    }
+}
---
+++ Assets/follow_player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class follow_player : MonoBehaviour {
+    public GameObject character;//got to drag and drop into unity
+    public GameObject Toggler_Control_inverter_camera_yview;
+    private Vector3 offset;
+    private Quaternion angle;
+    public Transform target;
+    public float turnspeed = 0.01f;
+    private float lookatchange;
+    public bool controlinverter = true;
+    private int invertermultiplier = 1;
+    public Scrollbar Scrollbar_MouseSpeed;
+
+
+    // Use this for initialization
+    void Start () {
+        //get where it's currintly pointing to start it off before gravity or mouse movement ruins it
+        offset = transform.position - character.transform.position;
+        angle =  character.transform.rotation;
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        controlinverter = Toggler_Control_inverter_camera_yview.GetComponent<Toggle>().isOn;
+        //inverter
+        if (controlinverter) { invertermultiplier = -1; } else { invertermultiplier = 1; }
+
+    }/// <summary>
+     /// this is super duper key!!!!! you have to put the void late update in right here otherwise it fights with the update breaking everything!!!!
+     /// </summary>
+    void LateUpdate()
+    {
+
+
+
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        //transform.rotation = character.transform.rotation * angle;
+
+        //Camera vertical rotation
+        lookatchange = Input.GetAxis("Mouse Y")*Scrollbar_MouseSpeed.value*7*invertermultiplier;
+        Debug.Log(transform.localRotation.x);
+        if (transform.localRotation.x > .145 & lookatchange > 0) { lookatchange = 0; Debug.Log("helloworld2"); }
+        
+        if (transform.localRotation.x < -.1450 & lookatchange < 0) { lookatchange = 0;Debug.Log("helloworld"); }
+        //look at change boolean inverter
+
+        transform.Rotate(new Vector3(lookatchange, 0 , 0));
+        //transform.position = character.transform.position + offset;
+        
+
+
+        //comented out this as a child
+        //transform.LookAt(new Vector3(target.position.x,target.position.y+lookatchange*invertermultiplier, target.position.z));
+
+        
+
+
+
+    }
+}
